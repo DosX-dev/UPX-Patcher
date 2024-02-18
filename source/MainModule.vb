5bbab16d1718ba52bf1aab93e607865b443610f2
DosX <admin@kay-software.ru>
2024-02-18 14:14:40 +0300

    Mutations upgraded

--- source/MainModule.vb
+++ source/MainModule.vb
@@ -51,72 +51,91 @@ Module Program
 
                 End If
 
+
+                Console.WriteLine("Sections confusing...")
+
+                bytesReplacer.PatchBytes(fileName, {&H55, &H50, ' #0
+                                                    &H58, &H30,
+                                                    &H0},
+                                                   Encoding.ASCII.GetBytes(".dosx"))
+
+                bytesReplacer.PatchBytes(fileName, {&H55, &H50, ' #1
+                                                    &H58, &H31,
+                                                    &H0},
+                                                   Encoding.ASCII.GetBytes(".fish"))
+
+                bytesReplacer.PatchBytes(fileName, {&H55, &H50, ' #2
+                                                    &H58, &H32,
+                                                    &H0},
+                                                   Encoding.ASCII.GetBytes(".code"))
+
+                Console.WriteLine("Version block confusing...")
+
+
+                Dim offset As Long = bytesReplacer.FindStringOffset(fileName, "UPX!") ' version identifier
+
+
+                If offset <> -1 Then
+                    Dim bytesToReplace As Int16 = 15,
+                        randomVersion(bytesToReplace) As Byte
+                    rnd.NextBytes(randomVersion)
+
+                    ' ?? ?? ?? ?? 00 'UPX!'
+
+                    bytesReplacer.PatchBytesByOffset(fileName,
+                                                     offset - bytesToReplace + 4, ' full version block
+                                                     randomVersion)
+                Else
+                    Throw New Exception("Can't get UPX version block offset.")
+                End If
+
+
+                '''''''''''''''''' legacy ''''''''''''''''''''''
+                ' Dim randomVersionId(6) As Byte
+                ' rnd.NextBytes(randomVersionId)
+                '
+                ' bytesReplacer.PatchBytes(fileName,
+                '                                 {
+                '                                     &H0, ' padding
+                '                                     &H55, &H50, &H58, ' "UPX"
+                '                                     &H21, ' "!"
+                '                                     &HD ' compressed data separator
+                '                                 }, randomVersionId)
+                ''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+                Console.WriteLine("Adding fake version block...")
+
+
+                bytesReplacer.PatchBytes(fileName,
+                                                {
+                                                    &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, ' padding
+                                                    &H0, &H0, &H0, &H0, ' 00 00 00 00 -> "DosX"
+                                                    &H0, ' version separator
+                                                    &H0, &H0, &H0, ' 00 00 00 -> "UPX"
+                                                    &H0, ' 00 -> "!"
+                                                    &H0 ' padding
+                                                }, {
+                                                    &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, ' padding
+                                                    &H44, &H6F, &H73, &H58, ' "DosX"
+                                                    &H0, ' version separator
+                                                    &H55, &H50, &H58, ' "UPX"
+                                                    &H21, ' "!"
+                                                    &H0 ' padding
+                                                }
+                )
+
+                Console.WriteLine("Replacing standart DOS Stub message...")
+
+                bytesReplacer.PatchBytes(fileName, Encoding.ASCII.GetBytes("This program cannot be run in DOS mode."),
+                                                   Encoding.ASCII.GetBytes("https://github.com/DosX-dev/UPX-Patcher"))
+
             Catch ex As Exception
                 Console.ForegroundColor = ConsoleColor.Red
                 Console.WriteLine(ex.Message)
                 Console.ResetColor()
                 Environment.Exit(1)
             End Try
-
-
-
-            Console.WriteLine("Sections confusing...")
-
-            bytesReplacer.PatchBytes(fileName, {&H55, &H50, ' #0
-                                                &H58, &H30},
-                                               Encoding.ASCII.GetBytes("DOSX"))
-
-            bytesReplacer.PatchBytes(fileName, {&H55, &H50, ' #1
-                                                &H58, &H31},
-                                               Encoding.ASCII.GetBytes("FISH"))
-
-            bytesReplacer.PatchBytes(fileName, {&H55, &H50, ' #2
-                                                &H58, &H32},
-                                               Encoding.ASCII.GetBytes("CODE"))
-
-            Console.WriteLine("Version block confusing...")
-
-            Dim randomVersionPrefix(3) As Byte
-            rnd.NextBytes(randomVersionPrefix)
-
-            bytesReplacer.PatchBytes(fileName,
-                                            {
-                                                &H33, &H2E, &H39 ' "3.9" ..
-                                            }, randomVersionPrefix)
-
-            Dim randomVersionId(6) As Byte
-            rnd.NextBytes(randomVersionId)
-
-            bytesReplacer.PatchBytes(fileName,
-                                            {
-                                                &H0, ' padding
-                                                &H55, &H50, &H58, ' "UPX"
-                                                &H21, ' "!"
-                                                &HD ' compressed data separator
-                                            }, randomVersionId)
-
-            Console.WriteLine("Adding fake version block...")
-
-            bytesReplacer.PatchBytes(fileName,
-                                            {
-                                                &H0, ' padding
-                                                &H0, &H0, &H0, &H0, ' 00 00 00 00 -> "DosX"
-                                                &H0, ' version separator
-                                                &H0, &H0, &H0, ' 00 00 00 -> "UPX"
-                                                &H0 ' 00 -> "!"
-                                            }, {
-                                                &H0, ' padding
-                                                &H44, &H6F, &H73, &H58, ' "DosX"
-                                                &H0, ' version separator
-                                                &H55, &H50, &H58, ' "UPX"
-                                                &H21 ' "!"
-                                            }
-            )
-
-            Console.WriteLine("Replacing standart DOS Stub message...")
-
-            bytesReplacer.PatchBytes(fileName, Encoding.ASCII.GetBytes("This program cannot be run in DOS mode."),
-                                               Encoding.ASCII.GetBytes("https://github.com/DosX-dev/UPX-Patcher"))
 
             Console.WriteLine("Done!")
         End If
--- source/Patcher.vb
+++ source/Patcher.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.IO
+Imports System.Text
 
 Class Patcher
     Public Function PatchBytes(ByVal filePath As String, ByVal pattern As Byte(), ByVal replacement As Byte()) As Boolean
@@ -84,6 +85,41 @@ Class Patcher
         Return False
     End Function
 
+    Function FindStringOffset(ByVal fileName As String, ByVal searchString As String) As Long
+        Dim offset As Long = -1
+        Dim searchBytes As Byte() = Encoding.UTF8.GetBytes(searchString)
 
+        Using fs As New FileStream(fileName, FileMode.Open, FileAccess.Read)
+            Dim buffer(searchBytes.Length - 1) As Byte
+            Dim bytesRead As Integer = fs.Read(buffer, 0, buffer.Length)
+            While bytesRead > 0
+                For i As Integer = 0 To bytesRead - searchBytes.Length
+                    Dim match As Boolean = True
+                    For j As Integer = 0 To searchBytes.Length - 1
+                        If buffer(i + j) <> searchBytes(j) Then
+                            match = False
+                            Exit For
+                        End If
+                    Next
+                    If match Then
+                        offset = fs.Position - bytesRead + i
+                        Return offset
+                    End If
+                Next
+                bytesRead = fs.Read(buffer, 0, buffer.Length)
+            End While
+        End Using
+
+        Return offset
+    End Function
+
+
+
+    Sub PatchBytesByOffset(ByVal fileName As String, ByVal offset As Long, ByVal replacementBytes As Byte())
+        Using fs As New FileStream(fileName, FileMode.Open, FileAccess.ReadWrite)
+            fs.Seek(offset, SeekOrigin.Begin)
+            fs.Write(replacementBytes, 0, replacementBytes.Length)
+        End Using
+    End Sub
 
 End Class
