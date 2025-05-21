770d13e602d4a1644d82da0a6c8cdd06d1be7ab0
DosX <admin@kay-software.ru>
2025-05-21 13:19:29 +0300

    Update Patcher.vb

--- source/Patcher.vb
+++ source/Patcher.vb
@@ -1,181 +1,176 @@
 Imports System.IO
 Imports System.Text
-Imports Microsoft.VisualBasic.Logging
 
-Module Program
-    Public rnd As New Random()
+Class Patcher
+    Public Function PatchBytes(ByVal filePath As String, ByVal pattern As Byte(), ByVal replacement As Byte()) As Boolean
 
-    Public Sub Main()
-        Console.ForegroundColor = ConsoleColor.DarkYellow
-        StdOut.Write(vbLf & " UPX-Patcher (", False)
-
-        Console.ForegroundColor = ConsoleColor.DarkCyan
-        StdOut.Write("https://github.com/DosX-dev/UPX-Patcher", False)
-
-        Console.ForegroundColor = ConsoleColor.DarkYellow
-        StdOut.Write(")" & vbLf, True)
-
-        Console.ResetColor()
-
-        Dim args = Environment.GetCommandLineArgs()
-
-        If args.Length = 1 Then
-            StdOut.Write("Usage: " & AppDomain.CurrentDomain.FriendlyName & " <file_path>", True)
-            Environment.Exit(0)
+        If Not File.Exists(filePath) Then
+            Return False
         End If
 
-        Dim fileName As String = args(1)
+        Dim matchFound As Boolean = False
 
-        If IO.File.Exists(fileName) Then
+        Using fileStream As New FileStream(filePath, FileMode.Open, FileAccess.ReadWrite, FileShare.None)
+            Dim buffer(123456) As Byte
+            Dim bytesRead As Integer = 0
 
+            Do
+                bytesRead = fileStream.Read(buffer, 0, buffer.Length)
+                For i As Integer = 0 To bytesRead - pattern.Length
+                    Dim match As Boolean = True
+                    For j As Integer = 0 To pattern.Length - 1
+                        If buffer(i + j) <> pattern(j) Then
+                            match = False
+                            Exit For
+                        End If
+                    Next
 
-            Dim bytesReplacer As New Patcher
-
-            Try
-
-                Using fs As New FileStream(fileName, FileMode.Open, FileAccess.Read)
-                    If Not New BinaryReader(fs).ReadBytes(2).SequenceEqual(New Byte() {&H4D, &H5A}) Then
-                        Throw New Exception("It doesn't look like a valid Windows executable.")
+                    If match Then
+                        fileStream.Position = fileStream.Position - bytesRead + i
+                        fileStream.Write(replacement, 0, replacement.Length)
+                        fileStream.Flush()
+                        matchFound = True
+                        Exit For
                     End If
-                End Using
+                Next
 
+                If matchFound Then Exit Do
+            Loop While bytesRead > 0
+        End Using
 
-                If Not bytesReplacer.isPatternPresent(fileName, {&H55, &H50, ' #0
-                                                                 &H58, &H30}) Then
+        Return matchFound
+    End Function
 
-                    If bytesReplacer.isPatternPresent(fileName, Encoding.ASCII.GetBytes("DosX")) Then
-                        Throw New Exception("This file already patched.")
-                    Else
-                        Throw New Exception("This file is not packed with UPX.")
-                    End If
-
-                End If
-
-
-                StdOut.Log("Sections confusing...")
-
-                bytesReplacer.PatchBytes(fileName, {&H55, &H50, ' #0
-                                                    &H58, &H30,
-                                                    &H0},
-                                                   Encoding.ASCII.GetBytes(".dosx"))
-
-                bytesReplacer.PatchBytes(fileName, {&H55, &H50, ' #1
-                                                    &H58, &H31,
-                                                    &H0},
-                                                   Encoding.ASCII.GetBytes(".fish"))
-
-                bytesReplacer.PatchBytes(fileName, {&H55, &H50, ' #2
-                                                    &H58, &H32,
-                                                    &H0},
-                                                   Encoding.ASCII.GetBytes(".code"))
-
-                StdOut.Log("Version block confusing...")
-
-
-                Dim offset As Long = bytesReplacer.FindStringOffset(fileName, "UPX!") ' version identifier
-
-                If offset <> -1 Then
-                    Dim bytesToReplace As Int16 = 12 + rnd.Next(1, 3),
-                        randomVersion(bytesToReplace) As Byte
-                    rnd.NextBytes(randomVersion)
-
-                    ' ?? ?? ?? ?? 00 'UPX!'
-
-                    bytesReplacer.PatchBytesByOffset(fileName,
-                                                     offset - bytesToReplace + 4, ' full version block
-                                                     randomVersion)
-                Else
-                    Throw New Exception("Can't get UPX version block offset.")
-                End If
-
-
-                '''''''''''''''''' legacy ''''''''''''''''''''''
-                ' Dim randomVersionId(6) As Byte
-                ' rnd.NextBytes(randomVersionId)
-                '
-                ' bytesReplacer.PatchBytes(fileName,
-                '                                 {
-                '                                     &H0, ' padding
-                '                                     &H55, &H50, &H58, ' "UPX"
-                '                                     &H21, ' "!"
-                '                                     &HD ' compressed data separator
-                '                                 }, randomVersionId)
-                ''''''''''''''''''''''''''''''''''''''''''''''''
-
-
-                '  StdOut.Log("Adding fake version block...")
-                '
-                '
-                '  bytesReplacer.PatchBytes(fileName,
-                '                                  {
-                '                                      &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, ' padding
-                '                                      &H0, &H0, &H0, &H0, ' 00 00 00 00 -> "DosX"
-                '                                      &H0, ' version separator
-                '                                      &H0, &H0, &H0, ' 00 00 00 -> "UPX"
-                '                                      &H0, ' 00 -> "!"
-                '                                      &H0 ' padding
-                '                                  }, {
-                '                                      &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, ' padding
-                '                                      &H44, &H6F, &H73, &H58, ' "DosX"
-                '                                      &H0, ' version separator
-                '                                      &H55, &H50, &H58, ' "UPX"
-                '                                      &H21, ' "!"
-                '                                      &H0 ' padding
-                '                                  }
-                '  )
-
-                StdOut.Log("Replacing standart DOS Stub message...")
-
-                bytesReplacer.PatchBytes(fileName, Encoding.ASCII.GetBytes("This program cannot be run in DOS mode."),
-                                                   Encoding.ASCII.GetBytes("https://github.com/DosX-dev/UPX-Patcher"))
-
-                StdOut.Log("WinAPI changing...")
-
-                bytesReplacer.PatchBytes(fileName, Encoding.ASCII.GetBytes("ExitProcess"), ' function name size is 11 bytes
-                                                   Encoding.ASCII.GetBytes("CopyContext"))
-
-                StdOut.Log("EntryPoint patching...")
-
-                Dim isBuild64 As Boolean = PE.Is64(fileName)
-
-                If isBuild64 Then
-                    bytesReplacer.PatchBytes(fileName, ' x86_64
-                                                    {
-                                                        &H53, ' push rbx
-                                                        &H56, ' push rsi [!!]
-                                                        &H57, ' push rdi
-                                                        &H55 ' push rbp
-                                                    },
-                                                    {
-                                                        &H53, ' push rbx
-                                                        &H57, ' push rdi [!!]
-                                                        &H56, ' push rsi
-                                                        &H55 ' push rbp
-                                                    }
-                                             )
-                Else
-                    bytesReplacer.PatchBytes(fileName, ' i386
-                                                    {
-                                                        &H0, ' db 0
-                                                        &H60, ' pushal [!!]
-                                                        &HBE ' mov esi
-                                                    },
-                                                    {
-                                                        &H0, ' db 0
-                                                        &H55, ' push ebp [!!]
-                                                        &HBE ' mov esi
-                                                    }
-                                             )
-                End If
-
-            Catch ex As Exception
-                Console.ForegroundColor = ConsoleColor.Red
-                StdOut.Write(ex.Message, True)
-                Console.ResetColor()
-                Environment.Exit(1)
-            End Try
-
-            StdOut.Log("Successfully patched!")
+    Function IsPatternPresent(filePath As String, pattern As Byte()) As Boolean
+        If Not File.Exists(filePath) Then
+            Return False
         End If
+
+        If pattern Is Nothing OrElse pattern.Length = 0 Then
+            Return False
+        End If
+
+        Using fs As New FileStream(filePath, FileMode.Open, FileAccess.Read)
+            If fs.Length < pattern.Length Then
+                Return False
+            End If
+
+            Dim buffer(fs.Length - 1) As Byte
+
+            Dim bytesRead As Integer = fs.Read(buffer, 0, buffer.Length)
+
+            While bytesRead > 0
+                If bytesRead >= pattern.Length Then
+                    For i As Integer = 0 To bytesRead - pattern.Length
+                        Dim match As Boolean = True
+
+                        For j As Integer = 0 To pattern.Length - 1
+                            If buffer(i + j) <> pattern(j) Then
+                                match = False
+                                Exit For
+                            End If
+                        Next
+
+                        If match Then
+                            Return True
+                        End If
+                    Next
+                End If
+
+                Array.Copy(buffer, pattern.Length, buffer, 0, bytesRead - pattern.Length)
+                bytesRead = fs.Read(buffer, bytesRead - pattern.Length, pattern.Length)
+            End While
+        End Using
+
+        Return False
+    End Function
+
+    Function FindStringOffset(ByVal fileName As String, ByVal searchString As String) As Long
+        Dim offset As Long = -1
+        Dim searchBytes As Byte() = Encoding.ASCII.GetBytes(searchString)
+
+        Using fs As New FileStream(fileName, FileMode.Open, FileAccess.Read)
+            Dim buffer(123456) As Byte,
+                bytesRead As Integer,
+                bufferOffset As Long = 0
+
+            Do
+                bytesRead = fs.Read(buffer, 0, buffer.Length)
+                If bytesRead > 0 Then
+                    For i As Integer = 0 To bytesRead - searchBytes.Length
+                        Dim match As Boolean = True
+                        For j As Integer = 0 To searchBytes.Length - 1
+                            If buffer(i + j) <> searchBytes(j) Then
+                                match = False
+                                Exit For
+                            End If
+                        Next
+                        If match Then
+                            offset = bufferOffset + i
+                            Return offset
+                        End If
+                    Next
+                    bufferOffset += bytesRead
+                End If
+            Loop While bytesRead > 0
+        End Using
+
+        Return offset
+    End Function
+
+
+
+    Sub PatchBytesByOffset(ByVal fileName As String, ByVal offset As Long, ByVal replacementBytes As Byte())
+        Using fs As New FileStream(fileName, FileMode.Open, FileAccess.ReadWrite)
+            fs.Seek(offset, SeekOrigin.Begin)
+            fs.Write(replacementBytes, 0, replacementBytes.Length)
+        End Using
     End Sub
-End Module
+
+
+    Public Function IndexOf(ByVal fileName As String, ByVal pattern() As Byte) As Integer
+        Dim fileStream As FileStream = Nothing
+        Try
+            fileStream = New FileStream(fileName, FileMode.Open, FileAccess.Read)
+
+            If pattern.Length > fileStream.Length Then Return -1
+
+            For Arr As Integer = 0 To fileStream.Length - pattern.Length - 1
+                Dim found As Boolean = True
+                For Searcher As Integer = 0 To (pattern.Length - 1)
+                    If fileStream.ReadByte() <> pattern(Searcher) Then
+                        found = False
+                        Exit For
+                    End If
+                Next
+                If found Then
+                    Return Arr
+                Else
+                    fileStream.Seek(Arr + 1, SeekOrigin.Begin)
+                End If
+            Next
+
+        Finally
+            If fileStream IsNot Nothing Then
+                fileStream.Close()
+            End If
+        End Try
+        Return -1
+    End Function
+
+    Public Function GetByte(ByVal fileName As String, ByVal index As Integer) As Byte
+        Dim fileStream As FileStream = Nothing
+        Try
+            fileStream = New FileStream(fileName, FileMode.Open, FileAccess.Read)
+            fileStream.Seek(index, SeekOrigin.Begin)
+
+            Return CByte(fileStream.ReadByte())
+
+        Finally
+            If fileStream IsNot Nothing Then
+                fileStream.Close()
+            End If
+        End Try
+    End Function
+
+End Class
